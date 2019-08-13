bf2dcba7f076a4c8e36894ba18b76197182cded6
Raghu <raghuvissapragada@gmail.com>
2019-08-13 17:52:04 +0530

    Removed pad subchild div

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 window.addEventListener('load', () => {
 	const sounds= document.querySelectorAll(".sound");
-	const pads= document.querySelectorAll(".pads div");
+	const pads= document.querySelectorAll("#paditem");
 	const visual= document.querySelector(".visual");
 	const colors = 	[
 						"#60d394", 
